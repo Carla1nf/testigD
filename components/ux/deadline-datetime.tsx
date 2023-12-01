609faa425e474b2be67182747992da983d531916
0xZod <0xZod@proton.me>
2023-12-01 20:47:24 +0000

    Fix when no time left

--- components/ux/deadline-datetime.tsx
+++ components/ux/deadline-datetime.tsx
@@ -3,10 +3,20 @@
 import { toDays, toHours } from "@/lib/display"
 
 const DaysHours = ({ deadline }: { deadline: number }) => {
+  const days = toDays(Number(deadline))
+  const hours = toHours(Number(deadline))
+
+  if (days === 0 && hours === 0) {
+    return (
+      <>
+        <span className="text-xs">--&nbsp;--</span>
+      </>
+    )
+  }
+
   return (
     <>
-      {toDays(Number(deadline))} <span className="text-xs">DAYS</span> {toHours(Number(deadline))}{" "}
-      <span className="text-xs">HOURS </span>
+      {days} <span className="text-xs">DAYS</span> {hours} <span className="text-xs">HOURS </span>
     </>
   )
 }
