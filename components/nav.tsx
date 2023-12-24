14a8ce1419b449e23aaa7b61e2c95cea79207300
Lopl <carlanft1@gmail.com>
2023-12-24 10:09:24 -0300

    add

--- components/nav.tsx
+++ components/nav.tsx
@@ -10,17 +10,17 @@ const Nav = () => {
         <Link href="/" className="w-36">
           <Image src="/files/navbar/debita.svg" height={50} width={90} alt="Dēbita" />
         </Link>
-        <Link className="px-6 text-center text-[14.5px] text-[#B6979C]" href="/dashboard">
+        <Link className="px-6 text-center text-[14.5px] h-12 items-center grid hover:bg-slate-400/5 text-[#B6979C]" href="/dashboard">
           Dashboard
         </Link>
-        <Link className="px-6 text-center text-[14.5px] text-[#B6979C]" href="/lend">
+        <Link className="px-6 text-center text-[14.5px] h-12 items-center grid text-[#B6979C]  hover:bg-slate-400/5" href="/lend">
           Lend
         </Link>
-        <Link className="px-6 text-center text-[14.5px] text-[#B6979C]" href="/borrow">
+        <Link className="px-6 text-center text-[14.5px] text-[#B6979C] h-12 items-center grid hover:bg-slate-400/5" href="/borrow">
           Borrow
         </Link>
         <a
-          className="px-6 text-center text-[14.5px] text-[#B6979C] flex items-center gap-1"
+          className="px-6 text-center text-[14.5px] text-[#B6979C] flex items-center gap-1 "
           href="https://paintswap.finance/marketplace/fantom/collections/0xCD2A61Da5Ef804C3D55636335F9c7482282571Dc"
           target="_blank"
           rel="noreferrer nofollow"
@@ -28,7 +28,7 @@ const Nav = () => {
           Debt Market <ExternalLink className="w-4 h-4" />
         </a>
       </div>
-      <Link className="px-4 text-center text-[14.5px] text-[#B6979C]" href="/create">
+      <Link className="px-4 text-center text-[14.5px] text-[#B6979C] h-12 items-center grid hover:bg-slate-400/5" href="/create">
         Create Offer
       </Link>
       <div className="flex flex-row justify-between items-center p-4">
