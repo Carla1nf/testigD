61045834e89ac7db99170171a04c10319cdfea24
0xZod <0xZod@proton.me>
2024-01-13 14:15:05 +0000

    fix svg warnings

--- components/icons.tsx
+++ components/icons.tsx
@@ -8,7 +8,6 @@ export const DashboardEqualizer = ({ className }: { className?: string }) => {
       viewBox="0 0 24 24"
       data-testid="DashboardEqualizer"
       className={className}
-      //   style="width: 120px; height: 120px; margin-top: 3px; color: rgb(166, 167, 102);"
     >
       <path d="M10 20h4V4h-4v16zm-6 0h4v-8H4v8zM16 9v11h4V9h-4z"></path>
     </svg>
@@ -23,7 +22,6 @@ export const DashboardAccountBalance = ({ className }: { className?: string }) =
       aria-hidden="true"
       viewBox="0 0 24 24"
       data-testid="DashboardAccountBalance"
-      //   style="width: 120px; height: 120px; margin-top: 3px; color: rgb(107, 102, 167);"
     >
       <path d="M4 10h3v7H4zm6.5 0h3v7h-3zM2 19h20v3H2zm15-9h3v7h-3zm-5-9L2 6v2h20V6z"></path>
     </svg>
@@ -32,14 +30,7 @@ export const DashboardAccountBalance = ({ className }: { className?: string }) =
 
 export const DashboardSavings = ({ className }: { className?: string }) => {
   return (
-    <svg
-      className={className}
-      focusable="false"
-      aria-hidden="true"
-      viewBox="0 0 24 24"
-      data-testid="DashboardSavings"
-      //   style="width: 120px; height: 120px; margin-top: 3px; color: rgb(167, 102, 102);"
-    >
+    <svg className={className} focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DashboardSavings">
       <path d="m19.83 7.5-2.27-2.27c.07-.42.18-.81.32-1.15.08-.18.12-.37.12-.58 0-.83-.67-1.5-1.5-1.5-1.64 0-3.09.79-4 2h-5C4.46 4 2 6.46 2 9.5S4.5 21 4.5 21H10v-2h2v2h5.5l1.68-5.59 2.82-.94V7.5h-2.17zM13 9H8V7h5v2zm3 2c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1z"></path>
     </svg>
   )
@@ -53,7 +44,6 @@ export const DashboardAccessAlarm = ({ className }: { className?: string }) => {
       aria-hidden="true"
       viewBox="0 0 24 24"
       data-testid="DashboardAccessAlarm"
-      //   style="width: 120px; height: 120px; margin-top: 3px; color: rgb(102, 167, 108);"
     >
       <path d="m22 5.72-4.6-3.86-1.29 1.53 4.6 3.86L22 5.72zM7.88 3.39 6.6 1.86 2 5.71l1.29 1.53 4.59-3.85zM12.5 8H11v6l4.75 2.85.75-1.23-4-2.37V8zM12 4c-4.97 0-9 4.03-9 9s4.02 9 9 9c4.97 0 9-4.03 9-9s-4.03-9-9-9zm0 16c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"></path>
     </svg>
@@ -63,7 +53,6 @@ export const DashboardAccessAlarm = ({ className }: { className?: string }) => {
 export const AvailableIcon = ({ className }: { className?: string }) => {
   return (
     <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="AvailableIcon" className={className}>
-      {/* <path d="M13 2.05v3.03c3.39.49 6 3.39 6 6.92 0 .9-.18 1.75-.48 2.54l2.6 1.53c.56-1.24.88-2.62.88-4.07 0-5.18-3.95-9.45-9-9.95zM12 19c-3.87 0-7-3.13-7-7 0-3.53 2.61-6.43 6-6.92V2.05c-5.06.5-9 4.76-9 9.95 0 5.52 4.47 10 9.99 10 3.31 0 6.24-1.61 8.06-4.09l-2.6-1.53C16.17 17.98 14.21 19 12 19z"></path> */}
       <path d="M16.53 11.06 15.47 10l-4.88 4.88-2.12-2.12-1.06 1.06L10.59 17l5.94-5.94zM19 3h-1V1h-2v2H8V1H6v2H5c-1.11 0-1.99.9-1.99 2L3 19c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V8h14v11z"></path>
     </svg>
   )
@@ -80,8 +69,6 @@ export const MarketSizeIcon = ({ className }: { className?: string }) => {
 export const TotalLentIcon = ({ className }: { className?: string }) => {
   return (
     <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="AvailableIcon" className={className}>
-      {/* <path d="M11.8 10.9c-2.27-.59-3-1.2-3-2.15 0-1.09 1.01-1.85 2.7-1.85 1.78 0 2.44.85 2.5 2.1h2.21c-.07-1.72-1.12-3.3-3.21-3.81V3h-3v2.16c-1.94.42-3.5 1.68-3.5 3.61 0 2.31 1.91 3.46 4.7 4.13 2.5.6 3 1.48 3 2.41 0 .69-.49 1.79-2.7 1.79-2.06 0-2.87-.92-2.98-2.1h-2.2c.12 2.19 1.76 3.42 3.68 3.83V21h3v-2.15c1.95-.37 3.5-1.5 3.5-3.55 0-2.84-2.43-3.81-4.7-4.4z"></path> */}
-      {/* <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm.88 15.76V19h-1.75v-1.29c-.74-.18-2.39-.77-3.02-2.96l1.65-.67c.06.22.58 2.09 2.4 2.09.93 0 1.98-.48 1.98-1.61 0-.96-.7-1.46-2.28-2.03-1.1-.39-3.35-1.03-3.35-3.31 0-.1.01-2.4 2.62-2.96V5h1.75v1.24c1.84.32 2.51 1.79 2.66 2.23l-1.58.67c-.11-.35-.59-1.34-1.9-1.34-.7 0-1.81.37-1.81 1.39 0 .95.86 1.31 2.64 1.9 2.4.83 3.01 2.05 3.01 3.45 0 2.63-2.5 3.13-3.02 3.22z"></path> */}
       <path d="M17 2H7c-1.1 0-2 .9-2 2v2c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 4H7V4h10v2zm3 16H4c-1.1 0-2-.9-2-2v-1h20v1c0 1.1-.9 2-2 2zm-1.47-11.81C18.21 9.47 17.49 9 16.7 9H7.3c-.79 0-1.51.47-1.83 1.19L2 18h20l-3.47-7.81zM9.5 16h-1c-.28 0-.5-.22-.5-.5s.22-.5.5-.5h1c.28 0 .5.22.5.5s-.22.5-.5.5zm0-2h-1c-.28 0-.5-.22-.5-.5s.22-.5.5-.5h1c.28 0 .5.22.5.5s-.22.5-.5.5zm0-2h-1c-.28 0-.5-.22-.5-.5s.22-.5.5-.5h1c.28 0 .5.22.5.5s-.22.5-.5.5zm3 4h-1c-.28 0-.5-.22-.5-.5s.22-.5.5-.5h1c.28 0 .5.22.5.5s-.22.5-.5.5zm0-2h-1c-.28 0-.5-.22-.5-.5s.22-.5.5-.5h1c.28 0 .5.22.5.5s-.22.5-.5.5zm0-2h-1c-.28 0-.5-.22-.5-.5s.22-.5.5-.5h1c.28 0 .5.22.5.5s-.22.5-.5.5zm3 4h-1c-.28 0-.5-.22-.5-.5s.22-.5.5-.5h1c.28 0 .5.22.5.5s-.22.5-.5.5zm0-2h-1c-.28 0-.5-.22-.5-.5s.22-.5.5-.5h1c.28 0 .5.22.5.5s-.22.5-.5.5zm0-2h-1c-.28 0-.5-.22-.5-.5s.22-.5.5-.5h1c.28 0 .5.22.5.5s-.22.5-.5.5z"></path>
     </svg>
   )
@@ -144,7 +131,7 @@ export const SpinnerIcon = ({ className }: { className?: string }) => {
       fill="none"
       viewBox="0 0 24 24"
     >
-      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
       <path
         className="opacity-75"
         fill="currentColor"
@@ -174,10 +161,10 @@ export const DebitaIcon = ({ className }: { className?: string }) => {
         x2="27.4894"
         y2="32.2261"
         stroke="url(#paint1_linear_52_736)"
-        stroke-width="3"
-        stroke-linecap="square"
+        strokeWidth="3"
+        strokeLinecap="square"
       />
-      <line x1="15.9946" y1="1.5" x2="27.4894" y2="1.5" stroke="#BC528C" stroke-width="3" stroke-linecap="square" />
+      <line x1="15.9946" y1="1.5" x2="27.4894" y2="1.5" stroke="#BC528C" strokeWidth="3" strokeLinecap="square" />
       <defs>
         <linearGradient
           id="paint0_linear_52_736"
@@ -188,7 +175,7 @@ export const DebitaIcon = ({ className }: { className?: string }) => {
           gradientUnits="userSpaceOnUse"
         >
           <stop stop-color="#D75071" />
-          <stop offset="1" stop-color="#705BDC" />
+          <stop offset="1" stopColor="#705BDC" />
         </linearGradient>
         <linearGradient
           id="paint1_linear_52_736"
@@ -198,8 +185,8 @@ export const DebitaIcon = ({ className }: { className?: string }) => {
           y2="32.7261"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#D75071" />
-          <stop offset="1" stop-color="#725AD8" />
+          <stop stopColor="#D75071" />
+          <stop offset="1" stopColor="#725AD8" />
         </linearGradient>
       </defs>
     </svg>
