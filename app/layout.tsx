b3c1b4b2dc2e2ce03570a79cbd177d1087fd480c
Lopl <carlanft1@gmail.com>
2024-03-25 22:40:13 +0100

    se

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-CQSQKXGX62"></script>
         <link rel="icon" type="image/svg+xml" href="/files/icon/debita-icon.svg" />
+        <script
+          id="google-analytics"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-CQSQKXGX62');
+  `,
+          }}
+        ></script>
       </head>
       <body className={cn("bg-background font-sans antialiased overflow-x-hidden h-[calc(100dvh)]", fontSans.variable)}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
